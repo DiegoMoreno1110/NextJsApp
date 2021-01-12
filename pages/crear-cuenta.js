6868f83cfc5448370eb101b24ae375ae9ac8ce75
DiegoMoreno <moreno.diego98@live.com.mx>
2021-01-11 19:02:38 -0600

    Crear cuenta view

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,16 +1,57 @@
 import React from 'react';
-import styled from '@emotion/styled';
+import {css} from '@emotion/react';
 import Layout from '../components/layout/Layout';
+import {Formulario, Campo, InputSubmit} from '../components/ui/Formulario';
 
-const CrearCuenta = () => {
-    return ( 
+const CrearCuenta = () => (
         <div>
-        <Layout>
-            <h1>Crear Cuenta</h1>
-        </Layout>
+            <Layout>
+                <>
+                    <h1
+                        css={
+                            css`
+                                text-align: center;
+                                margin-top: 5rem;
+                            `
+                        }
+                    >Crear Cuenta</h1>
+                    <Formulario>
+                        <Campo>
+                            <label htmlFor="nombre">Nombre</label>
+                            <input
+                                type="text"
+                                id="nombre"
+                                placeholder="Tu nombre"
+                                name="nombre"
+                            />
+                        </Campo>
+                        <Campo>
+                            <label htmlFor="email">Email</label>
+                            <input
+                                type="email"
+                                id="email"
+                                placeholder="Tu email"
+                                name="email"
+                            />
+                        </Campo>
+                        <Campo>
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                placeholder="Tu password"
+                                name="password"
+                            />
+                        </Campo>
+                        <InputSubmit
+                            type="submit"
+                            value="Crear cuenta"
+                        />
 
+                    </Formulario>
+                </>
+            </Layout>
         </div> 
-    );
-}
+)
  
 export default CrearCuenta;
